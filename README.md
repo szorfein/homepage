e7bfa33311379e50268855cb7413c0be41007956
szorfein <szorfein@gmail.com>
2018-03-03 13:10:07 +0100

    add base rss

--- README.md
+++ README.md
@@ -7,7 +7,26 @@ I will include some icons too. We'll see.
 
 # Configuration
 
+## Social 
+
 Edit `social-link.json`, you can find icon name to [ionicons.com](http://ionicons.com).
+
+## Rss 
+
+To edit `rss-feeds.json`, you have to generate url from [yahoo](https://developer.yahoo.com/yql/?dataTypeRadios=JSON)  
+
+Imagine, you want follow news from `http://threatpost.com/feed/`, you go to yahoo link above and can set a thing like this:
+
++ Response : `JSON`
++ Yql query : `select title,link from rss where url="http://threatpost.com/feed/" limit 3`
+
+Yql query is like a sql language.
+
+You receive a line like this:
+```
+https://query.yahooapis.com/v1/public/yql?q=select%20title%2Clink%20from%20rss%20where%20url%3D%22http%3A%2F%2Fthreatpost.com%2Ffeed%2F%22%20limit%203&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
+```
+And you past this line to `rss-feeds.json`.
 
 ## Troubleshooting
 
--- assets/js/app.js
+++ assets/js/app.js
@@ -16,4 +16,42 @@ $(document).ready(function() {
     });
     /* focus on #search at start */
     $('#search').focus();
+    /* Github: https://developer.github.com/v3/#json-p-callbacks */
+    /*
+    $.ajaxSetup({
+        url: 'https://api.github.com/users/szorfein',
+        type: 'GET',
+        dataType: 'jsonp'
+    });
+
+    $.ajax({
+        success: function(res) {
+            console.log('github request success')
+            var html = ''
+            html += '<div>';
+            html += '<img src="'+res.data.avatar_url+'" />';
+            html += '<p> Repos:'+res.data.public_repos+'</p>';
+            html += '<p> Followers:'+res.data.followers+'</p>';
+            html += '</div>';
+            $('#github').html(html);
+        },
+        error: function (xhr, status) {
+            console.log("error");
+        }
+    });
+    */
+    /* Rss */
+    // src = https://www.raymondcamden.com/2015/12/08/parsing-rss-feeds-in-javascript-options/
+    // Yahoo api : https://developer.yahoo.com/yql/?dataTypeRadios=JSON
+    var yql = "https://query.yahooapis.com/v1/public/yql?q=select%20title%2Clink%20from%20rss%20where%20url%3D%22http%3A%2F%2Fthreatpost.com%2Ffeed%2F%22%20limit%203&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+    $.getJSON(yql, function(res) {
+        data = res.query.results.item
+        html = '<p>'
+        console.log(res);
+        $.each(data, function(entryIndex, entry) {
+            html += '<a href="'+entry.link+'">'+entry.title + '</a><br />';
+        });
+        html += '</p>'
+        $('#rss-feeds').html(html)
+    }, "jsonp");
 });
